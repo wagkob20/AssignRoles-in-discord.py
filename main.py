fcd0077bbfc0e4dddf804511ca47c8c9084cee64
wagkob20 <94897208+wagkob20@users.noreply.github.com>
2022-06-12 15:33:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from pydoc import cli 
+import sys
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+intents.all()
+
+client = commands.Bot(command_prefix="?",intents = intents, help_command=None)
+
+@client.event
+async def on_ready():
+    print("Current Version:-", sys.version)
+    print("finished...")
+
+@client.command()
+@commands.has_permissions(manage_roles=True)
+async def asignRole(ctx, roleId : discord.Role):
+    server = client.get_guild(SERVER_ID)
+    role2 = discord.utils.get(server.roles, id = ROLE_ID) #Rolle, die vergeben wird
+    print("Role asigned to: ")
+    for guild in client.guilds:
+        for member in guild.members:
+            if roleId in member.roles:
+                print(member.name)
+                await member.add_roles(role2)  
+
+client.run("TOKEN")
